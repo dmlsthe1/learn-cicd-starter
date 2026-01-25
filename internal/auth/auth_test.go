3a4e0e9365a78714a4dc756350ccdf3c5a576208
Devin <dxangobiz@gmail.com>
2026-01-25 01:31:02 -0800

    Add unit tests for auth.go:GetAPIKey

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,30 @@
+package auth
+
+import (
+	"testing"
+	"net/http"
+)
+
+func TestGetAPIKey(t *testing.T) {
+	got, err := GetAPIKey(http.Header{"Authorization": []string{"ApiKey testKey"}})
+	if err != nil {
+		t.Fatalf("expected: no error, got: %v", err)
+	}
+	if got != "testKey" {
+		t.Fatalf("expected: %v, got: %v", "testKey", got)
+	}
+}
+
+func TestGetAPIKey_NoAuthHeader(t *testing.T) {
+	_, err := GetAPIKey(http.Header{})
+	if err != ErrNoAuthHeaderIncluded {
+		t.Fatalf("expected: %v, got: %v", ErrNoAuthHeaderIncluded, err)
+	}
+}
+
+func TestGetAPIKey_MalformedAuthHeader(t *testing.T) {
+	_, err := GetAPIKey(http.Header{"Authorization": []string{"Bearer testKey"}})
+	if err == nil || err.Error() != "malformed authorization header" {
+		t.Fatalf("expected: %v, got: %v", "malformed authorization header", err)
+	}
+}
